d91ec236a6c882336d7e22cd00b2aae3cdc593b3
maheravi <heravi.06ali@gmail.com>
2021-08-20 22:41:52 +0430

    Plot Function added

--- Perceptron_Classification_Class.py
+++ Perceptron_Classification_Class.py
@@ -1,7 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+from mpl_toolkits.mplot3d import Axes3D
+import IPython.display as IPdisplay
+import glob
+from PIL import Image as PIL_Image
+from images2gif import writeGif
 
 class Percepton:
     def __init__(self):
@@ -17,7 +21,6 @@ class Percepton:
         self.b = np.random.rand(1, 1)
         Error = []
         for i in range(self.X_train.shape[0]):
-
             y_pred = np.matmul(self.X_train[i], self.w) + self.b
             e = self.Y_train[i] - y_pred
 
@@ -32,12 +35,32 @@ class Percepton:
 
     def predict(self, X_test):
         Y_pred = np.matmul(X_test, self.w) + self.b
-        return Y_pred
+        predic = np.zeros(len(Y_pred))
+        for i, pred in enumerate(Y_pred):
+            if pred > 0:
+                predic[i] = (1)
+            elif pred < 0:
+                predic[i] = (-1)
+        return predic
 
-    def evaluate(self, data):
-        Y_pred = np.matmul(data, self.w) + self.b
-        error = np.mean(np.abs(data - Y_pred))
-        return error
+    def evaluate(self, X_test, Y_test):
+        Y_pred = np.matmul(X_test, self.w) + self.b
+        predic = np.zeros(len(Y_pred))
+        for i, pred in enumerate(Y_pred):
+            if pred > 0:
+                predic[i] = 1
+            elif pred < 0:
+                predic[i] = (-1)
+        accuracy = (predic == Y_test).sum() / len(Y_test)
+        error = np.mean(np.abs(Y_test - predic))
+        return error, accuracy
+
+    def pltlost(self, X_test, Y_test):
+        error = self.fit(X_test, Y_test)
+        error = np.array(error[0])
+        x = np.arange(0, X_test.shape[0])
+        plt.plot(x, error, marker='o')
+        plt.show()
 
 
 data1 = pd.read_csv('linear_data_train.csv')
@@ -50,8 +73,10 @@ Y_test = np.array(data2.iloc[:, 2])
 
 p = Percepton()
 Error, w, b = p.fit(X_train, Y_train)
-print(Error)
-EvalTrain = p.evaluate(X_train)
-print('Train Data Evaluation: ', EvalTrain)
-EvalTest = p.evaluate(X_test)
-print('Test Data Evaluation: ', EvalTest)
+prediction = p.predict(X_train)
+# print(prediction)
+EvalTrain, Accuracy = p.evaluate(X_train, Y_train)
+print('Train Data Evaluation: ', EvalTrain, 'Train Data Accuracy: ', Accuracy)
+EvalTest, Accuracy = p.evaluate(X_test, Y_test)
+print('Test Data Evaluation: ', EvalTest, 'Test Data Accuracy: ', Accuracy)
+p.pltlost(X_train, Y_train)
