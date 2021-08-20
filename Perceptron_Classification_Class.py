3051b0015672fa18b83a7e417d011ad78830f6d5
maheravi <heravi.06ali@gmail.com>
2021-08-20 23:10:19 +0430

    3D Plot added

--- Perceptron_Classification_Class.py
+++ Perceptron_Classification_Class.py
@@ -1,11 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from mpl_toolkits.mplot3d import Axes3D
-import IPython.display as IPdisplay
-import glob
-from PIL import Image as PIL_Image
-from images2gif import writeGif
+
 
 class Percepton:
     def __init__(self):
@@ -15,8 +11,11 @@ class Percepton:
         self.X_train = X_train
         self.Y_train = Y_train
         self.number_class = len(np.unique(Y_train))
+        x_range = np.arange(self.X_train[:, 0].min(), self.X_train[:, 0].max(), 0.01)
+        y_range = np.arange(self.X_train[:, 1].min(), self.X_train[:, 1].max(), 0.01)
+        x, y = np.meshgrid(x_range, y_range)
 
-        lr = 0.001
+        lr = 0.01
         self.w = np.random.rand(2, 1)
         self.b = np.random.rand(1, 1)
         Error = []
@@ -30,6 +29,17 @@ class Percepton:
             Y_pred = np.matmul(self.X_train, self.w) + self.b
             error = np.mean(np.abs(self.X_train - Y_pred))
             Error.append(error)
+
+            ax.clear()
+            z = self.w[0] * x + self.w[1] * y + self.b
+            ax.plot_surface(x, y, z, rstride=1, cstride=1, alpha=0.4)
+            ax.scatter(X_train[:, 0], X_train[:, 1], Y_train, c='r', marker='o')
+
+            ax.set_xlabel('Feature 1')
+            ax.set_ylabel('Feature 2')
+            ax.set_zlabel('Prediction')
+            plt.pause(0.1)
+        plt.show()
 
         return Error, self.w, self.b
 
@@ -70,6 +80,10 @@ X_train = np.matrix(data1.iloc[:, 0:2])
 Y_train = np.array(data1.iloc[:, 2])
 X_test = np.matrix(data2.iloc[:, 0:2])
 Y_test = np.array(data2.iloc[:, 2])
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.view_init(-140, 60)
 
 p = Percepton()
 Error, w, b = p.fit(X_train, Y_train)
